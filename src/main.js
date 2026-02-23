a2e1282c9eb5d26b7d62f23d1f5b516bda6816c7
Thomas-Klooster <thomaskloostert1@gmail.com>
2026-02-23 09:39:48 +0100

    vuetify setup

--- src/main.js
+++ src/main.js
@@ -4,9 +4,27 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Fonts
+import '@fontsource/roboto/100.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import '@fontsource/roboto/900.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 app.mount('#app')
