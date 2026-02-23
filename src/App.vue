b7b8cb3a07f653939377e76872859451f6b2cc8d
Thomas-Klooster <thomaskloostert1@gmail.com>
2026-02-23 13:15:07 +0100

    sidebar start

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
 <script setup>
+import { ref } from 'vue'
 
+const drawer = ref(false)
+const items = [
+  {
+    title: 'Dashboard',
+    disabled: false,
+    href: 'test',
+  },
+  {
+    title: 'Link 1',
+    disabled: false,
+    href: 'test',
+  },
+  {
+    title: 'Link 2',
+    disabled: true,
+    href: 'test',
+  },
+]
 </script>
 
 <template>
   <v-app>
-    <v-main>
+    <v-navigation-drawer v-model="drawer" permanent rounded @mouseleave="drawer = false">
+      <v-list density="compact" :opened="opened" @update:opened="opened = $event">
+        <v-list-item title="Notion Clone" subtitle="Built with Vuetify"></v-list-item>
+        <v-divider></v-divider>
+        <v-list-item link prepend-icon="mdi-home" title="Home"></v-list-item>
+        <v-list-item link prepend-icon="mdi-cog" title="Settings"></v-list-item>
+        <v-list-subheader>Recents</v-list-subheader>
+        <v-list-item prepend-icon="mdi-plus" title="Add New" link></v-list-item>
+        <v-list-subheader>Private</v-list-subheader>
+        <v-list-item prepend-icon="mdi-plus" title="Add New" link></v-list-item>
+        <v-list-subheader>Teamspaces</v-list-subheader>
+        <v-list-item prepend-icon="mdi-plus" title="Add New" link></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main @mouseenter="drawer = false">
+      <v-app-bar :elevation="0" density="compact">
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon @mouseenter="drawer = true"></v-app-bar-nav-icon>
+          <v-breadcrumbs :items="items"></v-breadcrumbs>
+        </template>
+      </v-app-bar>
       <v-container>
         <router-view />
-        <v-btn>
-          Button
-        </v-btn>
       </v-container>
     </v-main>
   </v-app>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,20 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import './assets/main.css'
+
+import axios from 'axios';
+axios.defaults.baseURL = '?';
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;   
 
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'   
 
 // Fonts
 import '@fontsource/roboto/100.css'
@@ -25,6 +33,14 @@ app.use(router)
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 })
+app.use(vuetify)
 
 app.mount('#app')
