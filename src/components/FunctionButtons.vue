2bd3ae727bd9860c0173cd72ec0885b7cdc0b267
Thomas-Klooster <thomaskloostert1@gmail.com>
2026-02-25 16:06:49 +0100

    final commit of the day

--- src/components/FunctionButtons.vue
+++ src/components/FunctionButtons.vue
@@ -1,54 +1,3 @@
-<template>
-    <div class="function-buttons-container">
-
-
-        <v-menu>
-
-            <template v-slot:activator="{ props: menu }">
-                <v-tooltip location="bottom" text="Delete, Duplicate and More...">
-                    <template v-slot:activator="{ props: tooltip }">
-                        <v-btn rounded icon size="small" density="compact" variant="text"
-                            v-bind="mergeProps(menu, tooltip)">
-                            <v-icon size="small">mdi-dots-horizontal</v-icon>
-                        </v-btn>
-                    </template>
-                </v-tooltip>
-            </template>
-
-
-            <v-list @mouseenter="drawer = true">
-                <v-list-item v-for="(item, index) in items" :key="index" :value="index" @mouseenter="drawer = true"
-                    style="user-select: none;">
-                    <v-list-item-title> {{ item.title }} </v-list-item-title>
-                </v-list-item>
-            </v-list>
-
-        </v-menu>
-
-
-        <v-tooltip location="bottom" text="Add a page inside">
-            <template v-slot:activator="{ props }">
-                <v-btn rounded icon size="small" density="compact" variant="text" v-bind="props">
-                    <v-icon size="small">mdi-plus</v-icon>
-                </v-btn>
-            </template>
-        </v-tooltip>
-    </div>
-</template>
-
-<script setup>
-import { mergeProps } from 'vue'
-
-const items = [
-    { title: 'Add to Favorites' },
-    { title: 'Copy link' },
-    { title: 'Duplicate' },
-    { title: 'Rename' },
-    { title: 'Move to' },
-    { title: 'Move to Trash' },
-]
-</script>
-
 <style>
 .function-buttons-container {
     visibility: hidden;
@@ -60,4 +9,62 @@ const items = [
     visibility: visible;
     opacity: 1;
 }
+
+.v-list-item__prepend .v-list-item__spacer {
+    width: 8px !important;
+}
 </style>
+
+<template>
+    <div class="function-buttons-container">
+        <v-menu>
+            <template v-slot:activator="{ props: menu }">
+                <v-tooltip location="bottom" text="Delete, Duplicate and More...">
+                    <template v-slot:activator="{ props: tooltip }">
+                        <v-btn rounded icon size="small" density="compact" variant="text" style="color: #5f5f5f"
+                            v-bind="mergeProps(menu, tooltip)">
+                            <v-icon size="small">mdi-dots-horizontal</v-icon>
+                        </v-btn>
+                    </template>
+                </v-tooltip>
+            </template>
+
+            <v-list @mouseenter="drawer = true" rounded="lg">
+                <template v-for="(item, index) in items" :key="index">
+                    <v-divider v-if="item.divider" style="margin: 0.5rem;" />
+
+                    <v-list-item v-else :value="index" @mouseenter="drawer = true" rounded
+                        style="user-select: none; min-width: 10rem; min-height: 1.25rem; margin-inline: 0.5rem">
+                        <template v-slot:prepend>
+                            <v-icon :icon="item.icon" size="small" />
+                        </template>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item>
+                </template>
+            </v-list>
+        </v-menu>
+
+        <v-tooltip location="bottom" text="Add a page inside">
+            <template v-slot:activator="{ props }">
+                <v-btn rounded icon size="small" density="compact" variant="text" v-bind="props" style="color: #5f5f5f">
+                    <v-icon size="small">mdi-plus</v-icon>
+                </v-btn>
+            </template>
+        </v-tooltip>
+    </div>
+</template>
+
+<script setup>
+import { mergeProps } from 'vue'
+
+const items = [
+    { title: 'Add to Favorites', icon: 'mdi-star' },
+    { divider: true },
+    { title: 'Copy link', icon: 'mdi-link-variant' },
+    { title: 'Duplicate', icon: 'mdi-checkbox-multiple-blank-outline' },
+    { title: 'Rename', icon: 'mdi-square-edit-outline' },
+    { divider: true },
+    { title: 'Move to', icon: 'mdi-arrow-right-top' },
+    { title: 'Move to Trash', icon: 'mdi-trash-can-outline' },
+]
+</script>
--- src/components/SidebarItem.vue
+++ src/components/SidebarItem.vue
@@ -3,16 +3,18 @@
         <v-list-item :style="{ paddingLeft: `${depth * 12 + 8}px` }" rounded="lg" class="sidebar-item"
             @click="handleClick" @mouseenter="isHovered = true" @mouseleave="isHovered = false">
             <template #prepend>
-                <v-btn v-if="page.children?.length || page.canHaveChildren"
-                    :icon="isHovered ? (isOpen ? 'mdi-chevron-down' : 'mdi-chevron-right') : page.icon" size="small"
-                    rounded variant="" density="compact" @click.stop="handleIconClick" />
-                <v-icon v-else size="small">
-                    {{ page.icon }}
+                <v-btn v-if="props.page.canHaveChildren" :icon="isHovered ? 'mdi-chevron-right' : props.page.icon"
+                    rounded
+                    :style="{ transform: isOpen ? 'rotate(35deg)' : 'rotate(0deg)', transition: 'transform 0.2s ease-in-out' }"
+                    style="color: #5f5f5f;" size="small" variant="text" density="compact"
+                    @click.stop="handleIconClick" />
+                <v-icon v-else size="small" style="color: #5f5f5f;">
+                    {{ props.page.icon }}
                 </v-icon>
             </template>
 
-            <v-list-item-title>
-                {{ page.title }}
+            <v-list-item-title style="color: #5f5f5f; user-select: none;">
+                {{ props.page.title }}
             </v-list-item-title>
 
             <template v-slot:append>
@@ -20,8 +22,17 @@
             </template>
         </v-list-item>
 
-        <template v-if="isOpen && page.children?.length">
-            <SidebarItem v-for="child in page.children" :key="child.id" :page="child" :depth="depth + 1" />
+        <template v-if="isOpen && props.page.canHaveChildren">
+            <template v-if="props.page.children?.length">
+                <SidebarItem v-for="child in props.page.children" :key="child.id" :page="child" :depth="depth + 1" />
+            </template>
+            <div v-else :style="{
+                paddingLeft: `${depth * 12 + 32}px`,
+                color: '#666',
+                fontSize: '0.875rem',
+            }">
+                No pages inside
+            </div>
         </template>
     </div>
 </template>
@@ -46,18 +57,17 @@ function handleClick() {
 }
 
 function handleIconClick() {
-    if (props.page.children?.length) {
-        isOpen.value = !isOpen.value
-    } else if (props.page.canHaveChildren) {
-        onAddChildPage(props.page)
-    }
+    isOpen.value = !isOpen.value
 }
 
 function navigateTo(page) {
     console.log('Navigate to:', page.title)
 }
-
-function onAddChildPage(page) {
-    console.log('Add page to:', page.title)
-}
 </script>
+
+<style scoped>
+:deep(.v-list-item--density-compact) {
+    min-height: 0 !important;
+    padding-block: 2px;
+}
+</style>
