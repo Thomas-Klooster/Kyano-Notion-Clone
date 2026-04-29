7918c7d6f5fd9a7302a7fd26013158834ae0bc8b
Aech <vincentbakker.08@gmail.com>
2026-04-29 12:07:03 +0200

    fix: refreshToken defined & location href to login after time passed

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
 
-// ? Creating a custom instance for request containing i believe sanctums stateful domain??
-
-//  !! DEBUG: if it doesn't work change it to be backend host
-// !! It should work on the frontend since the sanctum is on it the current host though..
 const api = axios.create({
      baseURL: 'http://localhost:8000/api',
      withCredentials: true,
@@ -18,51 +14,56 @@ const processQueue = (error, token = null) => {
      failedQueue = [];
 };
 
-
-// ? Adds a response interceptor
 api.interceptors.request.use((config) => {
      const token = localStorage.getItem('accessToken');
      if (token) config.headers.Authorization = `Bearer ${token}`;
      return config;
 })
 
-// ! Api constance responds to the interceptor request
 api.interceptors.response.use(
      (response) => response,
      async (error) => {
           const originalRequest = error.config;
 
+          if (originalRequest.url === '/refresh') {
+               localStorage.clear();
+               window.location.href = '/auth/login';
+               return Promise.reject(error);
+          }
+
           if (error.response?.status === 401 && !originalRequest._retry) {
                if (isRefreshing) {
-                    // TODO Do something with the with the response data
                     return new Promise((resolve, reject) => {
                     failedQueue.push({ resolve, reject});
                     }).then((token) => {
                     originalRequest.headers['Authorization'] = `Bearer ${token}`;
                     return api(originalRequest);
-                    })
+                    });
                }
+
                originalRequest._retry = true;
                isRefreshing = true;
 
                try {
-                    const { data } = await axios.post('http://localhost:8000/api/refresh', {
-                         refreshToken: localStorage.getItem('refreshToken'),
-                    });
+                    const refreshToken = localStorage.getItem('refreshToken');
+                const { data } = await api.post('/refresh', {}, {
+                    headers: { Authorization: `Bearer ${refreshToken}` },
+                });
+
                     localStorage.setItem('accessToken', data.accessToken);
+                    localStorage.setItem('refreshToken', data.refreshToken);
                     processQueue(null, data.accessToken);
                     return api(originalRequest);
-               } catch (error) {
-                    processQueue(error, null);
-                    // ? back to the loginpage after logout feature
+               } catch (refreshError) {
+                    processQueue(refreshError, null);
                     localStorage.clear();
                     window.location.href = '/auth/login';
-                    return Promise.reject(error);
+                    return Promise.reject(refreshError);
                } finally {
                     isRefreshing = false;
                }
           }
           return Promise.reject(error);
      }
-)
+);
 export default api;
